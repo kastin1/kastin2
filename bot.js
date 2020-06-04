ffdc4e9ffa8217e0b48e7a7701d365e3bd8ce264
kastin1 <66444953+kastin1@users.noreply.github.com>
2020-06-05 00:28:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['ID'];
+const myid = ['713227365039341579'];
 const prefix = ['$'];
 const client = new Discord.Client();
 const client2 = new Discord.Client();
